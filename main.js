8261f112112c708b9728193848e830b7fc214fee
Jessica Lord <jlord@github.com>
2016-08-19 21:52:10 -0700

    Fullscreen got annoying, add icon for Linux

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+var path = require('path')
+
 var electron = require('electron')
 var app = electron.app
 var BrowserWindow = electron.BrowserWindow
 
 var mainWindow
+var iconPath = path.join(__dirname, '/images/jsforcats.png')
 
 function createWindow () {
-  mainWindow = new BrowserWindow({fullscreen: true})
+  mainWindow = new BrowserWindow({width: 1000, height: 1000, title: 'JavaScript for Cats', icon: iconPath})
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   mainWindow.webContents.openDevTools({mode: 'bottom'})
